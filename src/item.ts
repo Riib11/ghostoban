44bc372f8afb78b097402d40a82b00f20a4aa9c5
Henry Blanchette <henry@monkeyface.com>
2022-08-27 21:46:13 +0000

    only set debug graphic if color is set

--- src/item.ts
+++ src/item.ts
@@ -1,37 +1,44 @@
-import * as ex from 'excalibur';
-import { Level } from './level';
-import { itemG } from './collision';
-import { ActorGraphic } from './ActorGraphic';
+import * as ex from "excalibur";
+import { Level } from "./level";
+import { itemG } from "./collision";
+import { ActorGraphic } from "./ActorGraphic";
 
 export class Item extends ActorGraphic {
   level: Level;
   points: ex.Vector[];
 
   constructor(args: {
-    level: Level,
-    name: string,
-    pos: ex.Vector,
-    points: ex.Vector[],
-    collisionType: ex.CollisionType,
-    collisionGroup?: ex.CollisionGroup,
-    offset: ex.Vector,
-    color: ex.Color,
+    level: Level;
+    name: string;
+    pos: ex.Vector;
+    points: ex.Vector[];
+    collisionType: ex.CollisionType;
+    collisionGroup?: ex.CollisionGroup;
+    offset: ex.Vector;
+    color?: ex.Color;
   }) {
     super({
       ...args,
-      collider: new ex.PolygonCollider({ points: args.points, offset: args.offset }),
-      collisionGroup: args.collisionGroup || itemG
+      collider: new ex.PolygonCollider({
+        points: args.points,
+        offset: args.offset,
+      }),
+      collisionGroup: args.collisionGroup || itemG,
     });
     this.level = args.level;
     this.points = args.points;
   }
 
   onInitialize(_engine: ex.Engine): void {
-    // TMP: this is probably just for debugging
-    this.graphics.use(new ex.Polygon({
-      points: this.points,
-      color: this.color,
-    }));
+    const color = this.color;
+    if (color !== undefined) {
+      // TMP: this is probably just for debugging
+      this.graphics.use(
+        new ex.Polygon({
+          points: this.points,
+          color: this.color,
+        })
+      );
+    }
   }
 }
-
