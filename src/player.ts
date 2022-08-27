0178d4f0ec65ddc9c9477a1ff52e53f4067a8b38
Jae Min Im <jaeminim@berkeley.edu>
2022-08-27 15:47:16 -0700

    player sprite and animation

--- src/player.ts
+++ src/player.ts
@@ -19,6 +19,7 @@ const offset = ex.vec(0, 0);
 export class Player extends ActorGraphic implements Damageable {
   level: Level;
   health: number;
+  direction: number;
 
   constructor(args: {
     level: Level,
@@ -35,6 +36,7 @@ export class Player extends ActorGraphic implements Damageable {
     this.health = 100;
     this.addChild(new PlayerController({ player: this, speed: 500 }));
     this.addChild(new Activator({ radius: 50, }));
+    this.direction = 1;
   }
 
   onInitialize(_engine: ex.Engine): void {
@@ -46,12 +48,18 @@ export class Player extends ActorGraphic implements Damageable {
     
     const idle_left = ex.Animation.fromSpriteSheet(player_sheet, [0], 0);
     const run_left = ex.Animation.fromSpriteSheet(player_sheet, [0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1], 100);
+    const idle_right = ex.Animation.fromSpriteSheet(player_sheet, [8], 0);
+    const run_right = ex.Animation.fromSpriteSheet(player_sheet, [8, 9, 10, 11, 12, 13, 14, 15, 14, 13, 12, 11, 10, 9], 100);
     idle_left.scale = new ex.Vector(3, 3);
     run_left.scale = new ex.Vector(3, 3);
+    idle_right.scale = new ex.Vector(3, 3);
+    run_right.scale = new ex.Vector(3, 3);
     this.graphics.add("idle_left", idle_left);
     this.graphics.add("run_left", run_left);
+    this.graphics.add("idle_right", idle_right);
+    this.graphics.add("run_right", run_right);
     
-    this.graphics.use("idle_left");
+    this.graphics.use("idle_right");
     
   }
   onPreUpdate(engine: ex.Engine, delta: number): void {
@@ -59,6 +67,30 @@ export class Player extends ActorGraphic implements Damageable {
     this.z = this.pos.y;
     if (engine.input.keyboard.isHeld(ex.Input.Keys.R)) {
       this.level.reset();
+    }
+  }
+
+  run_animation(movement: string) {
+    if (movement == "left") {
+      this.direction = -1;
+      movement = "run";
+    } else if (movement == "right") {
+      this.direction = 1;
+      movement = "run";
+    }
+    
+    if (movement == "idle") {
+      if (this.direction == 1) {
+        this.graphics.use("idle_right");
+      } else {
+        this.graphics.use("idle_left");
+      }
+    } else if (movement == "run") {
+      if (this.direction == 1) {
+        this.graphics.use("run_right");
+      } else {
+        this.graphics.use("run_left");
+      }
     }
   }
 
@@ -72,13 +104,13 @@ export class Player extends ActorGraphic implements Damageable {
 }
 
 
-const player_img = new ex.ImageSource('./src/resources/player/player_left.png');
+const player_img = new ex.ImageSource('./src/resources/player/player.png');
 player_img.load();
 
 const player_sheet = ex.SpriteSheet.fromImageSource({
     image: player_img,
     grid: {
-        rows: 1,
+        rows: 2,
         columns: 8,
         spriteWidth: 27,
         spriteHeight: 27
