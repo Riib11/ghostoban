c6c07ef34cfd861235a3361ffe9332d1093b7e67
Bretton <qsctr@users.noreply.github.com>
2022-08-27 23:19:43 -0700

    add bretton5

---
+++ src/level/Bretton5.ts
@@ -0,0 +1,33 @@
+import { Engine, vec } from "excalibur";
+import { Barrel } from "../item/Barrel";
+import { PressurePlate } from "../item/PressurePlate";
+import { Level } from "../level";
+
+export class Bretton5 extends Level {
+
+  constructor() {
+    super({
+      player_pos: vec(100, 100),
+      exit_pos: vec(950, 950)
+    });
+  }
+
+  onInitialize(engine: Engine): void {
+    super.onInitialize(engine);
+    this.addItem(new PressurePlate({
+      level: this,
+      pos: vec(500, 500),
+      onActivate() {
+        this.level.setExitActivated(true);
+      },
+      onDeactivate() {
+        this.level.setExitActivated(false);
+      }
+    }));
+    this.addItem(new Barrel({
+      level: this,
+      pos: vec(200, 900)
+    }));
+  }
+
+}
