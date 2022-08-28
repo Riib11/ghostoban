dad0f8acdbe63b781a05818781176392f74bb27a
Henry Blanchette <henry@monkeyface.com>
2022-08-28 00:32:16 -0700

    pushed

--- src/level/TutorialTelekinesis.ts
+++ src/level/TutorialTelekinesis.ts
@@ -18,40 +18,41 @@ export class TutorialTelekinesis extends Level {
   onInitialize(engine: ex.Engine): void {
     super.onInitialize(engine);
     // do stuff to setup level
-    
-    
+
+
     this.addWallLineH(ex.vec(50, 50), 10);
     this.addWallLineH(ex.vec(50, 950), 10);
     this.addWallLineV(ex.vec(50, 75), 9);
     this.addWallLineV(ex.vec(950, 75), 9);
-    
+
     this.addSpikes(ex.vec(125, 300), 4, 1);
     this.addSpikes(ex.vec(300, 125), 1, 4);
-    
+
     // this.addGhost(new CleaningGhost({
     //   level: this,
     //   pos: ex.vec(275, 450),
     //   speed: 300,
     //   items: objs,
     // }));
-    
+
     const barrel = new Barrel({ level: this, pos: ex.vec(500, 600) });
+    barrel.scale = ex.vec(0.5, 0.5);
     this.addItem(barrel);
-    
+
     this.addItem(new PressurePlate({
       level: this,
       pos: ex.vec(200, 200),
       onActivate: () => { this.setExitActivated(true) },
       onDeactivate: () => { this.setExitActivated(false) }
     }));
-    
+
     const ghost = new TelekinesisGhost({
       level: this,
       path: [ex.vec(250, 200), ex.vec(700, 200)],
       speed: 100
     });
     this.addGhost(ghost);
-    
+
     this.addAccessory(new Accessory({
       level: this,
       pos: ex.vec(400, 150),
